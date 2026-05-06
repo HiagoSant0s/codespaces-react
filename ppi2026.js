ef23803932b458cbfaddfd3c098a3cb0a8bf4560
HiagoSant0s <hiago.s@escolar.ifrn.edu.br>
2026-05-06 19:19:19 +0000

    Aula 06/05 - Introdução a Javascript

---
+++ ppi2026.js
@@ -0,0 +1,28 @@
+console.log("Bem vindo a PPI!");
+
+// variáveis
+let name = "João";
+let age = 17;
+let isStudant = true;
+
+const PI = 3.14; // n pode mudar de valor
+console.log(PI);
+
+// Operadores Aritiméticos
+let x = 5 + 5; // soma
+let y = "5" + 5; // concatenação
+let z = "Hello" + 5;
+
+console.log(x, y, z);
+console.log(typeof x);
+
+//ativar o formatador prettier ALT + SHIFT + F
+// operadores relacionais
+console.log('5' != 5); // diferença
+console.log('5' == 5); // compara tipo OU valor
+console.log('5' === 5); // compara tipo E valor
+
+//Operação de incremento
+console.log(x++);
+console.log(x);
+console.log(++x);
